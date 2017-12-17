3c13ae7132a1441352729c3371748c20e5ffde06
nicolas.dorier <nicolas.dorier@gmail.com>
2017-12-18 01:42:57 +0900

    Rewrite bitcoin config at every start

--- core/0.15.0.1/docker-entrypoint.sh
+++ core/0.15.0.1/docker-entrypoint.sh
@@ -4,14 +4,12 @@ set -e
 if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1" == "test_bitcoin" ]]; then
 	mkdir -p "$BITCOIN_DATA"
 
-	if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
-		cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
-		printtoconsole=1
-		rpcallowip=::/0
-		${BITCOIN_EXTRA_ARGS}
-		EOF
-		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
-	fi
+	cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
+	printtoconsole=1
+	rpcallowip=::/0
+	${BITCOIN_EXTRA_ARGS}
+	EOF
+	chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
@@ -21,6 +19,6 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
 
 	exec gosu bitcoin "$@"
+else
+	exec "$@"
 fi
-
-exec "$@"
